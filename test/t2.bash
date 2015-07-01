efe8ce58a91a1bd1ffc0f0ab9b4474997a560db5
Marc Mengel <mengel@fnal.gov>
2015-07-01 14:46:43 -0500

    fife_wrap fixups

--- test/t2.bash
+++ test/t2.bash
@@ -6,9 +6,9 @@ test_fife_wrap_getconfig() {
     export SAM_PROJECT=mwm_test_${$}-${t} 
     ifdh startProject $SAM_PROJECT $EXPERIMENT test-project mengel samdev
 
-    fife_wrap --source /scratch/mengel/p/setups --export FOO=bar --exe ./fake_exe.sh --getconfig --limit 3 2>&1 | tee out
+    fife_wrap --source /grid/fermiapp/common/products/etc/setups --export FOO=bar --exe ./fake_exe.sh --getconfig --limit 3 2>&1 | tee out
     count1=`grep '^fake_exe:' out | wc -l`
-    fife_wrap --source /scratch/mengel/p/setups --export FOO=bar --exe ./fake_exe.sh --getconfig --limit 2 2>&1 | tee out
+    fife_wrap --source /grid/fermiapp/common/products/etc/setups --export FOO=bar --exe ./fake_exe.sh --getconfig --limit 2 2>&1 | tee out
     count2=`grep '^fake_exe:' out | wc -l`
     echo "counts: " $count1 $count2
     [ $count1 = 3 -a $count2 = 2 ]
@@ -20,9 +20,9 @@ test_fife_wrap_multifile() {
     export SAM_PROJECT=mwm_test_${$}-${t} 
     ifdh startProject $SAM_PROJECT $EXPERIMENT test-project mengel samdev
 
-    fife_wrap --source /scratch/mengel/p/setups --export FOO=bar --exe ./fake_exe.sh --multifile --config fake.cfg --limit 3 2>&1 | tee out
+    fife_wrap --source /grid/fermiapp/products/common/etc/setups --export FOO=bar --exe ./fake_exe.sh --multifile --config fake.cfg --limit 3 2>&1 | tee out
     count1=`grep '^fake_exe:' out | wc -l`
-    fife_wrap --source /scratch/mengel/p/setups --export FOO=bar --exe ./fake_exe.sh --multifile --config fake.config --limit 2 2>&1 | tee out
+    fife_wrap --source /grid/fermiapp/products/common/etc/setups --export FOO=bar --exe ./fake_exe.sh --multifile --config fake.config --limit 2 2>&1 | tee out
     count2=`grep '^fake_exe:' out | wc -l`
     echo "counts: " $count1 $count2
     [ $count1 = 3 -a $count2 = 2 ]
@@ -34,9 +34,9 @@ test_fife_wrap_art() {
     export SAM_PROJECT=mwm_test_${$}-${t} 
     ifdh startProject $SAM_PROJECT $EXPERIMENT test-project mengel samdev
 
-    fife_wrap --source /scratch/mengel/p/setups --export FOO=bar --exe ./fake_exe.sh --ifdh_art --config fake.cfg --limit 3 2>&1 | tee out
+    fife_wrap --source /grid/fermiapp/products/common/etc/setups --export FOO=bar --exe ./fake_exe.sh --ifdh_art --config fake.cfg --limit 3 2>&1 | tee out
     count1=`grep '^fake_exe:' out | wc -l`
-    fife_wrap --source /scratch/mengel/p/setups --export FOO=bar --exe ./fake_exe.sh --ifdh_art --config fake.config --limit 2 2>&1 | tee out
+    fife_wrap --source /grid/fermiapp/products/common/etc/setups --export FOO=bar --exe ./fake_exe.sh --ifdh_art --config fake.config --limit 2 2>&1 | tee out
     count2=`grep '^fake_exe:' out | wc -l`
     echo "counts: " $count1 $count2
     [ $count1 = 1 -a $count2 = 1 ]
