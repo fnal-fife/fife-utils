d3c90cd0f5ec3c73cb0a0c6fc96b0bf1bead5c2e
Marc Mengel <mengel@fnal.gov>
2015-12-16 10:30:43 -0600

    don't forget to put in repository...

---
+++ libexec/clone_stripe.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+source /grid/fermiapp/products/common/etc/setups.sh
+#---------------
+setup fife_utils $FIFE_UTILS_VERSION
+#for now, while testing
+#cd /nova/app/users/mengel/fife_utils
+#setup -. fife_utils
+#setup awscli
+#export IFDH_DEBUG=1
+#printenv
+#set -x
+#---------------------
+# aws credentials...
+touch ${TMPDIR:-}/.awst
+chmod 600 ${TMPDIR:-}/.awst
+ifdh cp /pnfs/${GROUP}/scratch/users/${GRID_USER:-$USER}/awst ${TMPDIR:-/var/tmp}/.awst
+source ${TMPDIR:-}/.awst
+#---------------
+
+sam_clone_dataset --experiment=${SAM_EXPERIMENT} "$@"
