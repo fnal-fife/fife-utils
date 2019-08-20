101da43efdc8e9dd0d19f2d9019c42070a66a2dc
Marc Mengel <mengel@fnal.gov>
2019-08-20 13:59:06 -0500

    minor test framework fix

--- lib/fife_sam_utils.py
+++ lib/fife_sam_utils.py
@@ -869,8 +869,8 @@ def clone( d, dest, subdirf = twodeep, just_say=False, batch_size = 1, verbose =
 
     if kidlist:
        # we're the parent, and there are kids
-       for i in range(0,len(kidlist)-1):
-           os.wait()
+       for i in range(0,len(kidlist)):
+           os.waitpid(kidlist[i],0)
 
     if kidlist or int(ncopies) == 1:
        d.endProject(purl)
