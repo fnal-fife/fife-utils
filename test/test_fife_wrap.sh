8bbce22335a62cd011f059b80cb961b44acb1092
Marc Mengel <mengel@fnal.gov>
2019-11-26 11:16:34 -0600

    smoke tested --hash changes..

--- test/test_fife_wrap.sh
+++ test/test_fife_wrap.sh
@@ -55,6 +55,12 @@ test_env_meta() {
    ../libexec/fife_wrap --debug --find_setups --setup hypotcode --setup fife_utils --limit 4 --multifile --exe hypot.exe --postscript 'export INTENSITY=12.5' --addoutput foo.txt --rename unique --declare_metadata --add_metadata secondary.intensity=\$INTENSITY --add_location --add_to_dataset _poms_task --dest /pnfs/nova/scratch/users/mengel -- -o foo.txt -c
 }
 
-testsuite fife_wrap_tests -s setup_proj -t end_proj test_env_meta test_client_tmpl test_client_1 test_client_2 test_client_2a test_client_3 test_client_4 test_client_excl
+test_hash_dir() {
+    export POMS_TASK_ID=9999
+    ../libexec/fife_wrap --debug --find_setups --setup fife_utils --limit 1 --multifile  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_location --add_to_dataset _poms_task --dataset_exclude '*.xyzzy' --dest /pnfs/nova/scratch/users/mengel --hash 2 -- '>bar.root' '<' 
+}
+
+
+testsuite fife_wrap_tests -s setup_proj -t end_proj test_env_meta test_client_tmpl test_client_1 test_client_2 test_client_2a test_client_3 test_client_4 test_client_excl test_hash_dir
 
 fife_wrap_tests "$@"
