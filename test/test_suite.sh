70608859ffd84248f094eabb0d6506899dc4812d
Marc Mengel <mengel@fnal.gov>
2022-01-21 09:35:36 -0600

    add full suite wrapper

---
+++ test/test_suite.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+(
+    bash t1.bash -v
+    bash t1.bash --python3 -v
+    bash test_fife_wrap.sh -v
+    bash test_fife_wrap.sh --python3 -v
+    python test_mu2e_sam_prefix.py
+) 2>&1 | tee test_suite.out
