18c9b680aad20064e3cb7a68766604f7a952b0de
Marc Mengel <mengel@fnal.gov>
2024-08-05 17:03:39 -0500

    testing fixup

--- test/test_fife_wrap.sh
+++ test/test_fife_wrap.sh
@@ -10,8 +10,8 @@ esac
 setup_proj() {
     #echo "setup_proj: starting" >&3
     export EXPERIMENT=samdev
-   
     export SAM_PROJECT=mwm_`date +%Y%m%d%H%M%S`_$$
+    export IFDH_CP_MAXRETRIES=0    
     sleep 2
     cpurl=`ifdh startProject $SAM_PROJECT  samdev gen_cfg  mengel samdev `
     outdir=/tmp/out$$
@@ -38,57 +38,79 @@ end_proj() {
 
 test_ucondb_path() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --find_setups --dry_run --setup fife_utils --limit 1 --multifile --appname demo --appfamily demo --appvers demo  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_to_dataset _poms_task --add_location --dest 'https://dbdata0vm.fnal.gov:9443/mu2e_ucondb_prod/app/data/mwmtest' -- '>bar.root' '<' 
 }
 
 test_client_1() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --find_setups --setup fife_utils --limit 1 --multifile --appname demo --appfamily demo --appvers demo  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_to_dataset _poms_task --add_location --dest $workdir -- '>bar.root' '<' 
 }
 
 test_client_2() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --find_setups --setup fife_utils --prescript-unquote 'for%20i%20in%201%202%203%3B%20do%20echo%20%24i%20%3B%20done' --limit 1 --multifile --appname demo --appfamily demo --appvers demo  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_to_dataset _poms_task --add_location --dest $workdir -- '>bar.root' '<' 
 }
 test_client_2a() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --find_setups --setup fife_utils --prescript-unquote 'for%20i%20in%201%202%203%3B%20do%20echo%20%24i%20%3B%20done' --limit 1 --multifile --appname demo --appfamily demo --appvers demo  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_to_dataset _poms_analysis --add_location --dest $workdir -- '>bar.root' '<' 
 }
 test_client_3() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --find_setups --setup fife_utils --prescript-unquote 'for%20i%20in%201%202%203%3B%20do%20echo%20%24i%20%3B%20done' --limit 1 --multifile --appname demo --appfamily demo --appvers demo  --exe /bin/false --exe_stdout0=bar.root --addoutput bar.root --rename unique --declare_metadata --add_to_dataset _poms_task --add_location --dest $workdir 
     echo exit status $?
 }
 
 test_client_tmpl() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --find_setups --setup fife_utils --limit 1 --multifile --appname demo --appfamily demo --appvers demo  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_to_dataset _poms_task --add_location --dest $workdir -- '>bar.root' '<' 
 }
 test_client_4() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --find_setups --setup fife_utils --limit 4 --multifile --appname demo --appfamily demo --appvers demo  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_location --add_to_dataset _poms_task --dest $workdir -- '>bar.root' '<' 
 }
 
 test_pre_post_1() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --find_setups --setup fife_utils --limit 4 --multifile --appname demo --appfamily demo --appvers demo  --prescript-unquote="echo%20before" --exe cat --postscript-unquote="echo%20after" --  '<' 
 }
 
 
 test_client_excl() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --find_setups --setup fife_utils --limit 1 --multifile --appname demo --appfamily demo --appvers demo  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_location --add_to_dataset _poms_task --dataset_exclude '*.xyzzy' --dest $workdir -- '>bar.root' '<' 
 }
 
 test_env_meta() {
    export POMS_TASK_ID=9999
    export HYPOTCODE_FAST=1
+    rm -rf $workdir
+    mkdir $workdir
    ../libexec/fife_wrap --debug --find_setups --setup hypotcode --setup fife_utils --setup "ifdhc $IFDH_VERSION" --limit 4 --multifile --appname demo --appfamily demo --appvers demo --exe hypot.exe --postscript 'export INTENSITY=12.5' --addoutput foo.txt --rename unique --declare_metadata --add_metadata secondary.intensity=\$INTENSITY --add_location --add_to_dataset _poms_task --dest $workdir -- -o foo.txt -c
 }
 
 test_hash_dir() {
     export POMS_TASK_ID=9999
     mkdir /tmp/hashdir$$
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --export-unquote FOO%3d%22%27bar%27_%27baz%27%22 --find_setups --limit 1 --multifile --appname demo --appfamily demo --appvers v1_0  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_location --add_to_dataset _poms_task --dataset_exclude '*.xyzzy' --dest /tmp/hashdir$$ --hash 2 -- '>bar.root' '<' 
   res=$?
   ls -lR /tmp/hashdir$$
@@ -97,11 +119,15 @@ test_hash_dir() {
 
 test_hash_dir_sha() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --export-unquote FOO%3d%22%27bar%27_%27baz%27%22 --find_setups --setup fife_utils --limit 1 --multifile --appname demo --appfamily demo --appvers v1_0  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_location --add_to_dataset _poms_task --dataset_exclude '*.xyzzy' --dest $workdir --hash 2 --hash_alg sha256 -- '>bar.root' '<' 
 }
 
 test_parallel() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     mkdir /tmp/out$$
     ../libexec/fife_wrap --debug --limit 4 --multifile --appname demo --appfamily demo --appvers v1_0  --exe cat --addoutput 'bar*.root' --rename unique --declare_metadata --add_location --parallel=2 --dest=/tmp/out$$ -- '>bar${nthfile}.root' '<' 
     res=$?
@@ -112,6 +138,8 @@ test_parallel() {
 
 test_multi_format_path() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     mkdir /tmp/out$$
     ../libexec/fife_wrap --debug --limit 4 --multifile --appname demo --appfamily demo --appvers v1_0  --exe cat --addoutput 'bar*.root' --rename unique --declare_metadata --add_location --dest=/tmp/out$$/'${file_name[=7]}' -- '>bar${nthfile}.root' '<' 
     res=$?
@@ -121,9 +149,11 @@ test_multi_format_path() {
 }
 test_parallel_hashdir_lots() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     mkdir /tmp/out$$
     for i in 0 1 2 3 4 5 6 7 8 9; do for j in 0 1 2 3 4 5 6 7 8 9; do echo $i$j > bar$i.$j.root; done; done
-    IFDH_CP_MAXRETRIES=0 ../libexec/fife_wrap --debug --limit 5 --multifile --appname demo --appfamily demo --appvers v1_0  --exe cat --addoutput 'bar*.root' --rename unique --declare_metadata --add_location --parallel=2 --hash 2 --hash_alg sha256 --dest=/tmp/out$$ -- '>bar${nthfile}.root' '<' 
+    ../libexec/fife_wrap --debug --limit 5 --multifile --appname demo --appfamily demo --appvers v1_0  --exe cat --addoutput 'bar*.root' --rename unique --declare_metadata --add_location --parallel=2 --hash 2 --hash_alg sha256 --dest=/tmp/out$$ -- '>bar${nthfile}.root' '<' 
     res=$?
     ls -Rl /tmp/out$$
     rm -rf /tmp/out$$
@@ -133,6 +163,8 @@ test_parallel_hashdir_lots() {
 
 test_quot_env() {
     export POMS_TASK_ID=9999
+    rm -rf $workdir
+    mkdir $workdir
     ../libexec/fife_wrap --debug --export-unquote FOO%3d%60bar%60_%60baz%60 --find_setups --setup fife_utils --limit 4 --multifile --appname demo --appfamily demo --appvers v1_0  --exe cat --addoutput bar.root --rename unique --declare_metadata --add_location --add_to_dataset _poms_task --dataset_exclude '*.xyzzy' --dest $workdir --hash 2 -- '>bar.root' '<'  | tee tqe.out
 }
 
@@ -146,11 +178,14 @@ then
     . `ups setup -j ifdhc v2_6_1 -q +python36, ifdhc_config v2_6_1a`
     export IFDH_VERSION="v2_6_1 -q +python36"
 else
-    . `ups setup ifdhc`
+    . $(ups setup ifdhc)
+    . $(ups setup hypotcode)
 fi
 
 export EXPERIMENT=samdev
-export X509_USER_PROXY=`ifdh getProxy`
+export X509_USER_PROXY=$(ifdh getProxy)
+export BEARER_TOKEN_FILE=$(ifdh getToken)
+
 if [ ! -r /tmp/did_gen_cfg ]
 then
     $HYPOTCODE_DIR/bin/rebuild_gen_cfg
